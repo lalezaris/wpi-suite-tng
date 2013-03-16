16fc2e70d119e1ac3b09d5083d59b2f1d853d919
Tyler Stone <tjs_rocks@yahoo.com>
2013-03-16 15:22:05 -0400

    Added two new classes to a new view package, which cleans up some of the GUI code for use and viewing.

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/RequirementsManager.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/RequirementsManager.java
@@ -1,21 +1,17 @@
 package edu.wpi.cs.wpisuitetng.modules.RequirementsManager;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.ArrayList;
 
-import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.BorderFactory;
-
 import javax.swing.KeyStroke;
 
 import edu.wpi.cs.wpisuitetng.janeway.gui.widgets.KeyboardShortcut;
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.view.MainView;
+import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.view.ToolbarPanel;
 
 public class RequirementsManager implements IJanewayModule {
 	//initialize tabs
@@ -25,21 +21,14 @@ public class RequirementsManager implements IJanewayModule {
 	public RequirementsManager() {
 		tabs = new ArrayList<JanewayTabModel>();
 		
-		/**
-		 * create a JPanel to hold the toolbar for the tab
-		 * 
-		 * adapted from PostBoard tutorial by Tyler Stone
-		 */
-		JPanel toolbarPanel = new JPanel();
-		toolbarPanel.add(new JLabel("RM toolbar placeholder")); //label with placeholder text
-		toolbarPanel.setBorder(BorderFactory.createLineBorder(Color.blue, 2)); //add a border
-		//JPanel to hold contents of the tab
-		JPanel mainPanel = new JPanel();
-		mainPanel.add(new JLabel("RM placeholder"));
-		mainPanel.setBorder(BorderFactory.createLineBorder(Color.green, 2));
+		//new main panel
+		MainView mainView = new MainView();
+		
+		//new toolbar panel
+		ToolbarPanel toolbarPanel = new ToolbarPanel();
 	
 		//create a tab model that contains toolbar and content panel
-		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolbarPanel, mainPanel);
+		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolbarPanel, mainView);
 		
 		//add to the list of tabs
 		tabs.add(tab1);

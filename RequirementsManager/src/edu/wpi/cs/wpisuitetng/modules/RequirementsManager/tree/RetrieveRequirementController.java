2226871b179d97c0e734ffe28db7661c5fcc889b
Tyler Stone <tjs_rocks@yahoo.com>
2013-03-28 16:51:54 -0400

    Improved double clicking functionality for tree.

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/tree/RetrieveRequirementController.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/tree/RetrieveRequirementController.java
@@ -19,7 +19,12 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 
 import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.models.Requirement;
+import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.tree.RetrieveRequirementRequestObserver;
 import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.tabs.RequirementTable;
+import edu.wpi.cs.wpisuitetng.network.Network;
+import edu.wpi.cs.wpisuitetng.network.Request;
+import edu.wpi.cs.wpisuitetng.network.RequestObserver;
+import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
 /**
  * A mouse listener for the requirement table on the dashboard
@@ -40,11 +45,19 @@ public class RetrieveRequirementController extends MouseAdapter {
 	@Override
     public void mouseClicked(java.awt.event.MouseEvent evt) {
 		TreePath selectedPath = treeView.getTree().getSelectionPath();
-		Requirement selectedNode = (Requirement) ((ReqTreeNode)selectedPath.getLastPathComponent()).getRequirement();
+		int selRow = treeView.getTree().getRowForLocation(evt.getX(), evt.getY());
+		
+		if (selRow > -1) {
+			Requirement selectedNode = (Requirement) ((ReqTreeNode)selectedPath.getLastPathComponent()).getRequirement();
 
-		if (evt.getClickCount() == 2) {
-				showRequirement(selectedNode);
-        }
+			if (evt.getClickCount() == 2) {
+				final RequestObserver requestObserver = new RetrieveRequirementRequestObserver(this);
+				Request request;
+				request = Network.getInstance().makeRequest("requirementsmanager/requirement/" + selectedNode.getId(), HttpMethod.GET);
+				request.addObserver(new RetrieveRequirementRequestObserver(this));
+				request.send();
+        	}
+		}
 
     }
 	
@@ -54,5 +67,15 @@ public class RetrieveRequirementController extends MouseAdapter {
 	public void showRequirement(Requirement requirement) {
 		// Make a new defect view to display the defect that was received
 		treeView.getTabController().addEditRequirementTab(requirement);
+	}
+	
+	/**
+	 * Called by {@link RetrieveRequirementRequestObserver} when an error
+	 * occurred retrieving the requirement from the server.
+	 */
+	public void errorRetrievingRequirement(String error) {
+		JOptionPane.showMessageDialog(treeView, 
+				"An error occurred opening the defect you selected. " + error, "Error opening defect", 
+				JOptionPane.ERROR_MESSAGE);
 	}
 }

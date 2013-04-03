0ca3b4bb9118a2fc2c65f628249014c54df95e36
Tyler Stone <tjs_rocks@yahoo.com>
2013-04-03 03:39:48 -0400

    Rewrote the Jtree reqTreeModel class to allow for dynamic node addition

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/tree/ReqTreeModel.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/tree/ReqTreeModel.java
@@ -1,16 +1,18 @@
 /**************************************************
- * This file was developed for CS3733: Software Engineering
- * The course was taken at Worcester Polytechnic Institute.
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html 
- *
- * Contributors:
- *  Sam Lalezari
- *  Tushar Narayan
-**************************************************/
+ * This file was developed for CS3733: Software Engineering The course was taken
+ * at Worcester Polytechnic Institute.
+ * 
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Sam Lalezari
+ * 				 Tushar Narayan
+ * 				 Lauren Kahn
+ * 				 Tyler Stone
+ **************************************************/
+
 package edu.wpi.cs.wpisuitetng.modules.RequirementsManager.tree;
 
 import java.util.ArrayList;
@@ -22,7 +24,6 @@ import javax.swing.tree.MutableTreeNode;
 
 import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.models.Iteration;
 import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.models.Requirement;
-import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.models.RequirementStatus;
 import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.requirements.action.Refresher;
 import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.requirements.action.RefresherMode;
 import edu.wpi.cs.wpisuitetng.modules.RequirementsManager.requirements.controller.RetrieveAllChildRequirementsController;
@@ -33,18 +34,18 @@ import edu.wpi.cs.wpisuitetng.network.configuration.NetworkConfiguration;
 
 /**
  * The Model for the Requirement Tree
- *
- * @author Sam Lalezari
- * CHris Hanna changed this on Mar 27,2013
+ * 
+ * @author Sam Lalezari CHris Hanna changed this on Mar 27,2013
  * @version Mar 19, 2013
- *
+ * 
  */
-@SuppressWarnings({"serial"})
+@SuppressWarnings({ "serial" })
 public class ReqTreeModel extends DefaultTreeModel {
-	
+
 	DefaultMutableTreeNode root;
 	RetrieveAllRequirementsController controller;
 	RetrieveAllIterationsControllerTree itController;
+	RetrieveAllChildRequirementsController crcontroller;
 	LinkedList<ReqTreeNode> nodes = new LinkedList<ReqTreeNode>();
 	int count;
 	int id;
@@ -54,141 +55,97 @@ public class ReqTreeModel extends DefaultTreeModel {
 	Requirement[] childRequirements;
 	int parentCount = 0;
 	int childCount = 0;
-	RetrieveAllChildRequirementsController crcontroller;
-	
+
 	/**
 	 * Class constructor
 	 * 
-	 * @param root the root of the requirement tree
-	 * @param tree the tree of requirements
+	 * @param root
+	 *            the root of the requirement tree
+	 * @param tree
+	 *            the tree of requirements
 	 */
 	public ReqTreeModel(MutableTreeNode root) {
 		super(root);
-		Network.getInstance().setDefaultNetworkConfiguration(new NetworkConfiguration("http://wpisuitetng"));
+		Network.getInstance().setDefaultNetworkConfiguration(
+				new NetworkConfiguration("http://wpisuitetng"));
 		controller = new RetrieveAllRequirementsController(RefresherMode.TREE);
 		itController = new RetrieveAllIterationsControllerTree(this);
-		
+
 		this.root = (DefaultMutableTreeNode) root;
 		controller.refreshData();
 		crcontroller = new RetrieveAllChildRequirementsController();
 	}
 
 	/**
-	 * Fills the tree with requirements given in the array. 
-	 * Clears the existing tree first, so that there are no duplicates
+	 * Fills the tree with requirements given in the array. Clears the existing
+	 * tree first, so that there are no duplicates
 	 * 
 	 * @param reqs
 	 */
-	public void fillTree(Requirement[] reqs){
-		
-		System.out.println("Filling the tree");
-		if (reqs != null){
+	public void fillTree(Requirement[] reqs) {
+		if (reqs != null) {
 			requirements = reqs;
-		}
-		
-		//ReqTreeNode temp = null;
-		count = 0;
-		id = 0;
-		
-		//CHris Hanna added this line
-		this.iterations = Refresher.getInstance().getInstantIterations();
-		this.root.removeAllChildren();
-		
-		this.reload();
-		// Loop through all the iterations
-		for (int j = 0; j < iterations.length; j++) {
-			DefaultMutableTreeNode tempIt = new DefaultMutableTreeNode(
-					iterations[j]);
-			this.insertNodeInto(tempIt, root, 0);
-			count++;
-			
-			System.out.println("Added iteration with ID = " + iterations[j].getId());
-			
-			for (int k = 0 ; k < iterations[j].getRequirements().size(); k++){
-				
-				int reqId = iterations[j].getRequirements().get(k);
-				System.out.println("Req id is:" + reqId);
-				
-				DefaultMutableTreeNode parentNode = null;
-				DefaultMutableTreeNode childNode = null;
-				
-				for (int r = 0 ; r < requirements.length; r ++){
-					System.out.println("Top level: " + requirements[r].isTopLevelRequirement());
-					if(requirements[r].isTopLevelRequirement()){
-						System.out.println("HEREEEEEEEEEEEEEEEEEEEEEEEEEE");
-					if (reqId == requirements[r].getId() && requirements[r].getStatus() != RequirementStatus.DELETED){
-						System.out.println("Iter" + iterations[j].getId() + " has Req" + requirements[r].getId());
-						//parentRequirements[parentCount] = requirements[r];
-						//parentCount++;
-						if(parentNode == null)
-							parentNode = new DefaultMutableTreeNode(requirements[r]);
-						else parentNode.add(new DefaultMutableTreeNode(requirements[r]));
-						System.out.println("Before childrennnnnnnnnnnn");
-						//for(int p = 0; p < parentRequirements.length; p++){
-						System.out.println("CRcontroller: " + requirements[r].getId());
-							ArrayList<Requirement> children = crcontroller.retrieveChildrenByID(requirements[r].getId());
-							if (children != null){
-								System.out.println("Children!");
-							for (Requirement currentChild : children){
-								if(currentChild.getStatus() != RequirementStatus.DELETED){
-									if(childNode == null)
-										childNode = new DefaultMutableTreeNode(requirements[r]);
-									else childNode.add(new DefaultMutableTreeNode(requirements[r]));
-								}
-							}
-							}
-							if(childNode != null){
-								this.insertNodeInto(childNode, parentNode, 0);
-							}
-						//}
-					}
-					}
-				}
-				
-				if(parentNode != null){
-					this.insertNodeInto(parentNode, tempIt, 0);
-					
-					count++;
-				}
-				/**for (int c = 0; c < parentRequirements.length; c++){
-					if (parentNode == null)
-						parentNode = new DefaultMutableTreeNode(parentRequirements[c]);
-					else parentNode.add(new DefaultMutableTreeNode(parentRequirements[c]));
-				if (parentNode != null){
-					this.insertNodeInto(parentNode, tempIt, 0);
-					
-					count++;
-				}
-				}
-				
-				for (int i = 0; i < parentRequirements.length; i++){
-					Requirement[] children = new Requirement[crcontroller.retrieveChildrenByID(parentRequirements[i].getId()).length];
-					for(int n = 0; n < children.length; n++){
-						if(children[n].getStatus() != RequirementStatus.DELETED){
-							childRequirements[childCount] = children[n];
-							childCount++;
-						}
 
+			// ReqTreeNode temp = null;
+			count = 0;
+			id = 0;
+
+			// CHris Hanna added this line
+			this.iterations = Refresher.getInstance().getInstantIterations();
+			this.root.removeAllChildren();
+
+			this.reload();
+
+			LinkedList<DefaultMutableTreeNode> iterationNodes = new LinkedList<DefaultMutableTreeNode>();
+			LinkedList<DefaultMutableTreeNode> nodes = new LinkedList<DefaultMutableTreeNode>();
+
+			for (int r = 0; r < iterations.length; r++) {
+				// initialize all new iteration nodes
+				iterationNodes.add(new DefaultMutableTreeNode(iterations[r]));
+			}
+
+			for (int r = 0; r < requirements.length; r++) {
+				// initialize all new tree nodes
+				nodes.add(new DefaultMutableTreeNode(requirements[r]));
+			}
+
+			for (int r = 0; r < requirements.length; r++) { // iterate through every requirement
+				Requirement leafReq = (Requirement) nodes.get(r).getUserObject();
+
+				if (!(leafReq.getParentRequirementId() == -1)) { // check if it isn't top level
+					int parentID = leafReq.getParentRequirementId(); // get parent id
+					for (int z = 0; z < requirements.length; z++) { // iterate through all reqs
+						Requirement potentialParent = (Requirement) nodes.get(z).getUserObject();
+
+						if (potentialParent.getId() == parentID) {
+							nodes.get(z).add(nodes.get(r));
+						}
+					}
+				} else {
+
+					int reqIterationID = leafReq.getIterationId();
+					for (int z = 0; z < iterations.length; z++) {
+						Iteration potentialIteration = (Iteration) iterationNodes.get(z).getUserObject();
+
+						if (potentialIteration.getId() == reqIterationID) {
+							iterationNodes.get(z).add(nodes.get(r));
+						}
 					}
 				}
-				
-				for (int m = 0; m < childRequirements.length; m++){
-					if(childNode == null)
-						childNode = new DefaultMutableTreeNode(childRequirements[m]);
-					else childNode.add(new DefaultMutableTreeNode(childRequirements[m]));
-					if(childNode != null){
-						this.insertNodeInto(childNode, parentNode, 0);
-					}
-				} */
 			}
+
+			for (int r = 0; r < iterations.length; r++) {
+				root.add(iterationNodes.get(r));
+			}
+
+			TreeView.expandAll();
 		}
-		TreeView.expandAll();
 	}
-	
+
 	/**
 	 * Refresh the tree.
 	 */
-	public void refreshTree(){
+	public void refreshTree() {
 		controller.refreshData();
 		itController.refreshData();
 	}
@@ -202,5 +159,4 @@ public class ReqTreeModel extends DefaultTreeModel {
 		this.iterations = iterations;
 		this.fillTree(null);
 	}
-	
 }

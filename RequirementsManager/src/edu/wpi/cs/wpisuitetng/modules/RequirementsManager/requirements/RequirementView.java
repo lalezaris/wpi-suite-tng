4d8acd88e398de2dd5cefaf85c3bbbff3e9903c1
Chris Dunkers <cmdunkers@gmail.com>
2013-04-25 23:11:21 -0400

    fixed the view when the requirement is in a past iteration

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/requirements/RequirementView.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementsManager/requirements/RequirementView.java
@@ -461,6 +461,24 @@ public class RequirementView extends JPanel implements IToolbarGroupProvider {
 			mainPanel.disableFields(new JComponent[]{mainPanel.getDeleteRequirementBottom()});
 			mainPanel.getDeleteRequirementBottom().setToolTipText("Cannot delete this requirement as it has children.");
 		} 
+		
+		Calendar cStart = Calendar.getInstance();
+		cStart.setTime(Iteration.getIterationById(reqModel.getUneditedRequirement().getIterationId()).getStartDate());
+		Calendar cEnd = Calendar.getInstance();
+		cEnd.setTime(Iteration.getIterationById(reqModel.getUneditedRequirement().getIterationId()).getEndDate());
+		cEnd.add(Calendar.DATE, 1);
+		Calendar cNow = Calendar.getInstance();
+		if (cEnd.compareTo(cNow) < 0 && reqModel.getUneditedRequirement().getIterationId() != Iteration.getBacklog().getId()){
+			mainPanel.disableFields(new JComponent[]{mainPanel.getCmbPriority(),mainPanel.getCmbType(),mainPanel.getTxtDescription(),mainPanel.getTxtEstimate(),mainPanel.getTxtActual(),mainPanel.getTxtCreator(),/*txtAssignee,*/
+					mainPanel.getTxtTitle(),mainPanel.getTxtReleaseNumber(),mainPanel.getCmbIteration(),mainPanel.getNotesView().getSaveButton(),mainPanel.getNotesView().getTextArea(),
+					mainPanel.getAssigneeView().getBtnAdd(), mainPanel.getAssigneeView().getBtnRemove(),mainPanel.getAcceptanceTestsView().getListDisplay()});
+			mainPanel.changeBackground(new JTextComponent[]{mainPanel.getTxtDescription(),mainPanel.getTxtEstimate(),mainPanel.getTxtActual(),mainPanel.getTxtCreator(),/*txtAssignee,*/
+					mainPanel.getTxtTitle(),mainPanel.getTxtReleaseNumber(),mainPanel.getNotesView().getTextArea()});
+			mainPanel.makeTextBlack(new JTextComponent[]{mainPanel.getTxtDescription(),mainPanel.getTxtEstimate(),mainPanel.getTxtActual(),mainPanel.getTxtCreator(),/*txtAssignee,*/
+					mainPanel.getTxtTitle(),mainPanel.getTxtReleaseNumber()});
+			mainPanel.makeStuffNotVisible(new JComponent[]{mainPanel.getCreateChildRequirement()});
+		}
+		
 	}
 
 	/**
